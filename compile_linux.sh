8d37365c706a46bdb17f2e812384b5c42df53cea
Shish <shish@shishnet.org>
2013-05-24 00:18:08 +0100

    script to make linux binaries

---
+++ compile_linux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
+../pyinstaller-2.0/pyinstaller.py --onefile --icon icon.ico --name eve-mlp eve_mlp/main_cli.py
+../pyinstaller-2.0/pyinstaller.py --onefile --icon icon.ico --name eve-gmlp eve_mlp/main_gui.py --windowed 
