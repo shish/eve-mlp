59929826bc974230dd2adf3f14f6ba7bd2d29437
Shish <shish@shishnet.org>
2013-05-24 00:36:42 +0100

    gui designs

--- eve_mlp/gui.py
+++ eve_mlp/gui.py
@@ -1,8 +1,34 @@
 import sys
-import logging    
+import logging
 from Tkinter import *
 
+
 log = logging.getLogger(__name__)
+
+
+#
+# if passwords saved:
+#    Enter master password
+#    [                   ]
+#                   Unlock
+#
+# main screen:
+#    User  Pass          
+#    Abc   ***   [Launch]
+#    Def   ***   [Launch]
+#    [    Launch All    ]
+#
+#    menu:
+#       server
+#          tranquility (live)
+#          singularity (beta)
+#       options
+#          locate eve install
+#          locate singularity install
+#          (x) remember passwords
+#       tools
+#          launch official patcher
+#
 
 def main(args=sys.argv):
     root = Tk()
