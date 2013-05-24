43c88db0b059e9b3723819e3a3ab40ae34ef9eb6
Shish <shish@shishnet.org>
2013-05-24 14:50:11 +0100

    place for more news tabs

--- eve_mlp/gui.py
+++ eve_mlp/gui.py
@@ -140,23 +140,28 @@ class LauncherPanel(wx.Panel):
 
 
 class NewsPanel(wx.Panel):
+    def add_http_panel(self, url, name):
+#        https://client.eveonline.com/launcherv3
+#        DIV.overview>ARTICLE.news
+        html = wx.html.HtmlWindow(self.nb)
+        try:
+            # TODO: async load
+            data = requests.get(url).text
+        except Exception as e:
+            data = "Couldn't get %s:\n%s" % (url, str(e))
+        html.SetPage(data)
+        self.nb.AddPage(html, name)
+
     def __init__(self, parent, config):
         wx.Panel.__init__(self, parent)
         self.config = config
 
-        nb = wx.Notebook(self)
+        self.nb = wx.Notebook(self)
 
-        self.html = wx.html.HtmlWindow(nb)
-        try:
-            # TODO: async load
-            data = requests.get("http://code.shishnet.org/eve-mlp/news.html").text
-        except Exception as e:
-            data = "Couldn't get news: %s" % str(e)
-        self.html.SetPage(data)
-        nb.AddPage(self.html, "MLP")
+        self.add_http_panel("http://code.shishnet.org/eve-mlp/news.html", "MLP News")
 
         box = wx.StaticBoxSizer(wx.StaticBox(self, label="News"), wx.VERTICAL)
-        box.Add(nb, 1, wx.EXPAND)
+        box.Add(self.nb, 1, wx.EXPAND)
         self.SetSizer(box)
         self.Layout()
 
