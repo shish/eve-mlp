9de35bf0c4d7c9a435e9ca59a36dd6cbda3bfa19
Shish <shish@shishnet.org>
2013-05-24 19:25:30 +0100

    don't crash if icon.ico is missing

--- compile_win32.bat
+++ compile_win32.bat
@@ -1,4 +1,4 @@
 C:\Python27\python.exe ..\pyinstaller-2.0\pyinstaller.py --onefile --icon icon.ico --name eve-mlp eve_mlp/main_cli.py
-C:\Python27\python.exe ..\pyinstaller-2.0\pyinstaller.py --onefile --icon icon.ico --name eve-gmlp eve_mlp/main_gui.py --windowed 
+C:\Python27\python.exe ..\pyinstaller-2.0\pyinstaller.py --onefile --icon icon.ico --name eve-gmlp eve_mlp/main_gui.py --windowed --resource icon.ico --resource LICENSE.txt
 
 pause
