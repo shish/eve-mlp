70c51badeed493b8a7804a333359c58f9bb4893d
Shish <shish@shishnet.org>
2013-05-25 09:54:09 +0100

    make non-standard python install path work too

--- compile_win32.bat
+++ compile_win32.bat
@@ -1,4 +1,9 @@
-C:\Python27\python.exe ..\pyinstaller-2.0\pyinstaller.py --onefile --icon icon.ico --name eve-mlp eve_mlp/main_cli.py
-C:\Python27\python.exe ..\pyinstaller-2.0\pyinstaller.py --onefile --icon icon.ico --name eve-gmlp eve_mlp/main_gui.py --windowed --resource icon.ico --resource LICENSE.txt
+@echo off
+
+REM For people who don't have python in their path, assume it exists in the default location as a fallback
+SET PATH=%PATH%;C:\Python27
+
+python.exe ..\pyinstaller-2.0\pyinstaller.py --onefile --icon icon.ico --name eve-mlp eve_mlp/main_cli.py
+python.exe ..\pyinstaller-2.0\pyinstaller.py --onefile --icon icon.ico --name eve-gmlp eve_mlp/main_gui.py --windowed --resource icon.ico --resource LICENSE.txt
 
 pause
