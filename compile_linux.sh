7a0109cf111b914ecab9e01a9eb58ecceb3d4295
Shish <shish@shishnet.org>
2013-06-04 16:27:39 +0100

    hand-build a .spec file to include data files

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 export VERSION=`git describe | sed s/v//`
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
-../pyinstaller-2.0/pyinstaller.py --onefile --icon icon.ico --name eve-mlp main_cli.py
-../pyinstaller-2.0/pyinstaller.py --onefile --icon icon.ico --name eve-gmlp main_gui.py --windowed
+../pyinstaller-2.0/pyinstaller.py compile_generic.spec
 
 cd dist
 cp ../LICENSE.txt ../icon.ico ../help.html ./
--- compile_win32.bat
+++ compile_win32.bat
@@ -3,7 +3,6 @@
 REM For people who don't have python in their path, assume it exists in the default location as a fallback
 SET PATH=%PATH%;C:\Python27
 
-python.exe ..\pyinstaller-2.0\pyinstaller.py --onefile --icon icon.ico --name eve-mlp main_cli.py
-python.exe ..\pyinstaller-2.0\pyinstaller.py --onefile --icon icon.ico --name eve-gmlp main_gui.py --windowed
+python.exe ..\pyinstaller-2.0\pyinstaller.py compile_generic.spec
 
 pause
