f4efc0932f19afb8c75bb391265aa1317f08a7a3
Shish <shish@shishnet.org>
2013-06-04 16:33:42 +0100

    no more need to bundle extra files in the tarball

--- compile_linux.sh
+++ compile_linux.sh
@@ -4,6 +4,5 @@ export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
 ../pyinstaller-2.0/pyinstaller.py compile_generic.spec
 
 cd dist
-cp ../LICENSE.txt ../icon.ico ../help.html ./
-tar cvzf eve-mlp-$VERSION.tar.gz eve-mlp LICENSE.txt
-tar cvzf eve-gmlp-$VERSION.tar.gz eve-gmlp LICENSE.txt icon.ico help.html
+tar cvzf eve-mlp-$VERSION.tar.gz eve-mlp
+tar cvzf eve-gmlp-$VERSION.tar.gz eve-gmlp
